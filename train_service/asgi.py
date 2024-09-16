f39282d26f9cf9936f5fe70b6ee650bfd00c508c
Pobedilda <playfog3@gmail.com>
2024-09-16 15:51:50 +0300

    refactor: change name for main app

--- train_service/asgi.py
+++ train_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for train_api project.
+ASGI config for train_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "train_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "train_service.settings")
 
 application = get_asgi_application()
