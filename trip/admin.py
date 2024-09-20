552b50c12dead02495c2989fb223c0c2d2ff1b67
Pobedilda <playfog3@gmail.com>
2024-09-20 17:10:01 +0300

    add: register models

--- trip/admin.py
+++ trip/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from trip.models import TrainType, Ticket, Journey, Crew, Route, Station, Order, Train
+
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Order)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Journey)
+admin.site.register(Ticket)
