5a2a8beb1db4e5251e8ed593618d9137c9072179
Pobedilda <playfog3@gmail.com>
2024-09-27 14:09:32 +0300

    add: Django Secret Key to env

--- train_service/settings.py
+++ train_service/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8op#x)^(bv=2a-3pwrnx8379+hb7r&y8neg5g1p%r_w4d1hqpp"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
